cf0364a1b7857beb7ed6012d45861096076bf5fc
ggung <ggung2485@gmail.com>
2025-06-05 15:24:52 +0900

    Fix relative paths for GitHub Pages

--- js/book.js
+++ js/book.js
@@ -15,6 +15,9 @@ $(document).ready(function () {
         // 페이지 넘김 감지
 
         let page4Visited = false;
+        let page5Visited = false;
+        let page6Visited = false;
+        let page7Visited = false;
         function updateFlipbookCentering(page) {
             const lastPage = $("#flipbook").turn("pages");
 
@@ -28,28 +31,72 @@ $(document).ready(function () {
                 console.log("비디오 페이지 (3페이지) - 가운데 정렬 OFF");
             } else if (page === 4) {
                 // ⭐ 처음 1회만 실행되도록 조건 추가
-                    if (!page4Visited) {
-                        page4Visited = true;
+                if (!page4Visited) {
+                    page4Visited = true;
 
-                        $(".fade-in").removeClass("show").text("");
+                    $(".fade-in").removeClass("show").text("");
 
-                        setTimeout(() => {
-                            $("#line1").text("하지만 우주에선").addClass("show");
-                        }, 500);
-                        setTimeout(() => {
-                            $("#line2").text("모든게 가벼워 진다는데").addClass("show");
-                        }, 1500);
-                        setTimeout(() => {
-                            $("#line3").text("내 작은 친구가").addClass("show");
-                        }, 2500);
-                        setTimeout(() => {
-                            $("#line4").text("민들레 홀씨처럼 날아가 버릴까").addClass("show");
-                        }, 3500);
-                        setTimeout(() => {
-                            $("#line5").text("걱정이 되었어.").addClass("show");
-                        }, 4500);
-                    }
+                    setTimeout(() => {
+                        $("#line1").text("강아지별은 아주 멋진 곳이래.").addClass("show");
+                    }, 100);
+                    setTimeout(() => {
+                        $("#line2").text("디디가 좋아하는 풀입과").addClass("show");
+                    }, 1500);
+                    setTimeout(() => {
+                        $("#line3").text("부드러운 바람,").addClass("show");
+                    }, 2500);
+                    setTimeout(() => {
+                        $("#line4").text("맘껏 뛰어놀 수 있는 동산은").addClass("show");
+                    }, 3500);
+                    setTimeout(() => {
+                        $("#line5").text("어디든 땅을 파도 혼나지 않아.").addClass("show");
+                    }, 4500);
+                }
+            } else if (page === 6) {
+                // ⭐ 처음 1회만 실행되도록 조건 추가
+                if (!page5Visited) {
+                    page5Visited = true;
 
+                    $(".fade-in").removeClass("show").text("");
+
+                    setTimeout(() => {
+                        $("#line6").text("하지만 우주에선").addClass("show");
+                    }, 100);
+                    setTimeout(() => {
+                        $("#line7").text("모든게 가벼워 진다는데").addClass("show");
+                    }, 1500);
+                    setTimeout(() => {
+                        $("#line8").text("내 작은 친구가").addClass("show");
+                    }, 2500);
+                    setTimeout(() => {
+                        $("#line9").text("민들레 홀씨처럼 날아가 버릴까").addClass("show");
+                    }, 3500);
+                    setTimeout(() => {
+                        $("#line10").text("걱정이 되었어.").addClass("show");
+                    }, 4500);
+                } 
+            } else if (page === 8) {
+                // ⭐ 처음 1회만 실행되도록 조건 추가
+                if (!page6Visited) {
+                    page6Visited = true;
+
+                    $(".fade-in").removeClass("show").text("");
+
+                    setTimeout(() => {
+                        $("#line11")
+                            .html('<img src="./img/KakaoTalk_20250605_145039910.gif" alt="나비gif" width="100" />')
+                            .addClass("show");
+                    }, 100);
+                    setTimeout(() => {
+                        $("#line12").text("그때, 작은 나비 한 마리가").addClass("show");
+                    }, 1500);
+                    setTimeout(() => {
+                        $("#line13").text("어둠 속에서 나타났어.").addClass("show");
+                    }, 2500);
+                }  
+            
+                $("#flipbook .page video").addClass("on");   
+        
             } else if (page === lastPage) {
                 $("#flipbook").removeClass("on").addClass("off");
                 $("#flipbook .page video").addClass("on");
